9cfd62a69912a28750a5e2fcad519bb3d8e4132e
Fareed Quraishi <fareedq@fareedqs-macbook-pro.local>
2016-01-03 13:58:45 -0500

    Adjusted settings page so that the time picker would collapse

--- TheFeelz/SettingsVC.swift
+++ TheFeelz/SettingsVC.swift
@@ -9,13 +9,87 @@
 import UIKit
 
 class SettingsVC: UIViewController {
-
+    
+    @IBOutlet weak var sleepDatePicker: UIDatePicker!
+    @IBOutlet weak var wakeDatePicker: UIDatePicker!
+    @IBOutlet weak var wakeTitleLabel: UILabel!
+    @IBOutlet weak var wakeView: UIView!
+    @IBOutlet weak var wakeViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var sleepTitleLabel: UILabel!
+    @IBOutlet weak var sleepView: UIView!
+    @IBOutlet weak var sleepViewHeightConstraint: NSLayoutConstraint!
+    
+    var sleepHidden = true
+    var wakeHidden = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        hideSleepDP()
+        hideWakeDP()
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: "tapGesture:")
+        self.view.addGestureRecognizer(gestureRecognizer)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    
+    func tapGesture(sender: UITapGestureRecognizer){
+        
+        if sleepTitleLabel.frame.contains(sender.locationInView(view)) {
+            if sleepHidden {
+                sleepHidden = false
+                UIView.animateWithDuration(0.3, animations: { () -> Void in
+                    self.hideWakeDP()
+                    self.showSleepDP()
+                    self.view.layoutIfNeeded()
+                })
+                wakeHidden = true
+            } else {
+                UIView.animateWithDuration(0.3, animations: { () -> Void in
+                    self.hideSleepDP()
+                    self.view.layoutIfNeeded()
+                })
+                sleepHidden = true
+            }
+        }
+        
+        if wakeTitleLabel.frame.contains(sender.locationInView(view)) {
+            if wakeHidden {
+                wakeHidden = false
+                UIView.animateWithDuration(0.3, animations: { () -> Void in
+                    self.hideSleepDP()
+                    self.showWakeDP()
+                    self.view.layoutIfNeeded()
+                })
+                sleepHidden = true
+            } else {
+                UIView.animateWithDuration(0.3, animations: { () -> Void in
+                    self.hideWakeDP()
+                    self.view.layoutIfNeeded()
+                })
+                wakeHidden = true
+            }
+        }
+    }
+    
+    func hideWakeDP(){
+        wakeView.alpha = 0
+        wakeViewHeightConstraint.constant = 0
+    }
+    
+    func showWakeDP() {
+        wakeView.alpha = 1
+        wakeViewHeightConstraint.constant = 185
+    }
+    
+    func hideSleepDP(){
+        sleepView.alpha = 0
+        sleepViewHeightConstraint.constant = 0
+    }
+    
+    func showSleepDP(){
+        sleepView.alpha = 1
+        sleepViewHeightConstraint.constant = 185
     }
     
     func setupTimeBasedNotification(){
@@ -28,5 +102,8 @@ class SettingsVC: UIViewController {
 //        localNotification.fireDate = datePicker.date
 //        
 //        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+    }
+    @IBAction func sleepDatePickerAction(sender: UIDatePicker) {
+        sleepTitleLabel.text = "I go to sleep at \(sender.date)"
     }
 }
