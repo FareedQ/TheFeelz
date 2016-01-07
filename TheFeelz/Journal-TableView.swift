d30be08b69f8f7e8a6e4080859a752b12cd1022f
Fareed Quraishi <fareedq@fareedqs-macbook-pro.local>
2016-01-07 15:46:58 -0500

    Added a hack to fix the issue with the last question cell in the journal copying the response of the first

--- TheFeelz/Journal-TableView.swift
+++ TheFeelz/Journal-TableView.swift
@@ -64,6 +64,11 @@ extension JournalVC: UITableViewDataSource, UITableViewDelegate  {
         tempCell.backgroundColor = Feelz.sharedInstance.getBackgroundColour()
         tempCell.questionLabel.textColor = Feelz.sharedInstance.getFontColour()
         tempCell.inputTextFeild.delegate = self
+        
+        //had a problem with the final textfield kept loading the value from the first one. Needed to put this hack in place for the demo day
+        if indexPath.row == arrayOfQuestions.count - 1 {
+            tempCell.inputTextFeild.text = ""
+        }
     }
     
 }
